2058a03a12e1dc1a487b3ce9b7798d28fd1f6da2
moslem parve <muslimparvi@gmail.com>
2025-01-03 16:34:37 +0500

    routes

---
+++ Contorllers/UserController.js
@@ -0,0 +1,21 @@
+// userController.js
+import { validationResult } from 'express-validator';
+
+export const welcomeMessage = (req, res) => {
+  res.json({ message: 'Welcome to the node js' });
+};
+
+export const getUsers = (req, res) => {
+  res.json({ users: ['Alice', 'Bob', 'Charlie'] });
+};
+
+export const createUser = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, age } = req.body;
+  console.log(`Received Name: ${name}, Age: ${age}`);
+  res.status(201).json({ message: 'User created successfully', user: { name, age } });
+};
--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,17 @@
 import { Router } from 'express';
 import { body, validationResult } from 'express-validator';
+import { welcomeMessage, getUsers, createUser } from '../Contorllers/UserController.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the node js' });
-});
 
-router.get('/users', (req, res) => {
-  res.json({ users: ['Alice', 'Bob', 'Charlie'] });
-});
+router.get('/', welcomeMessage);
+
+router.get('/users',getUsers);
 
 router.post('/users',[
   body('name').notEmpty().withMessage('Name is required').isString().withMessage('Name must be a string'),
   body('age').notEmpty().withMessage('Age is required').isInt({ min: 1 }).withMessage('Age must be a positive integer'),
-],(req,res)=>{
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const { name, age } = req.body;
-  console.log(`Received Name: ${name}, Age: ${age}`);
-
-  res.json({
-    message: 'Data received successfully',
-    receivedData: { name, age },
-  });
-});
+],createUser);
 
 export default router;
